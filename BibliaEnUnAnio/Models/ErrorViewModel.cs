2f4d7f399f5e1b9d744332ffe5a326eb88a54265
Gerardo <gavilesf@gmail.com>
2021-12-17 14:48:35 -0600

    adding project

---
+++ BibliaEnUnAnio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BibliaEnUnAnio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
